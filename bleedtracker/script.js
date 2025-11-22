e928921758a947f86981f367ba9c164f417a5766
deviyl <73563489+deviyl@users.noreply.github.com>
2025-11-22 11:52:31 -0600

    Update script.js

--- bleedtracker/script.js
+++ bleedtracker/script.js
@@ -15,8 +15,8 @@ async function loadAttacks() {
       const row = document.createElement('tr');
       row.innerHTML = `
         <td>${attack.id}</td>
-        <td>${new Date(attack.started * 1000).toLocaleString()}</td>
-        <td>${new Date(attack.ended * 1000).toLocaleString()}</td>
+        <td>${new Date(attack.started * 1000).toUTCString()}</td>
+        <td>${new Date(attack.ended * 1000).toUTCString()}</td>
         <td>${attackerName}</td>
         <td>${attackerFaction}</td>
         <td>${defenderName}</td>
